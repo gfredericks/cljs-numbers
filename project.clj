7ace87804e736259464808d8cc0b43f3c4f766cd
Gary Fredericks <fredericksgary@gmail.com>
2013-01-05 11:42:38 -0600

    Upgrade libs

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cljs-numbers "1.0.0-SNAPSHOT"
+(defproject cljs-numbers "1.0.0"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [compojure "1.0.3"]]
-  :plugins [[lein-cljsbuild "0.1.10"]
+  :plugins [[lein-cljsbuild "0.2.10"]
             [lein-ring "0.7.0"]]
   :ring {:handler cljs-numbers.repl/handler}
   :cljsbuild {:builds [{:source-path "src-cljs"
